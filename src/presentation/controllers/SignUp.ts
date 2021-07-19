3b4acc1c9ea07e3b3e7614707d989f77b45c6bd3
Norman Frieman <ezequiel-d.s@hotmail.com>
2021-07-19 09:29:00 -0300

    feat: ensure SignUpController return 400 if password confirmation fails

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -17,6 +17,10 @@ export class SignUpController implements Controller{
                 }
             }
 
+            if(httpRequest.body.password !== httpRequest.body.passwordConfirmation){
+                return new BadRequest(new InvalidParamError('passwordConfirmation'));
+            }
+
             const isValid = this.emailValidator.isValid(httpRequest.body.email);
             if(!isValid){
                 return new BadRequest(new InvalidParamError('email'));
