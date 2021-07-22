6644a71fee8ec32f49282f09c1e52713e0265505
Norman Frieman <ezequiel-d.s@hotmail.com>
2021-07-21 22:31:40 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usercases/add-account/db-add-account.spec.ts
+++ src/data/usercases/add-account/db-add-account.spec.ts
@@ -89,5 +89,19 @@ describe('DbAddAccount Usecase', () => {
             email: 'valid_mail@mail.com',
             password: 'hashed_password'
         });
+    }),
+    test('Should throw if AddAccountRepository throws', async () => {
+        const { sut, addAccountRepositoryStub } = makeSut();
+        
+        jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+
+        const accountData: AddAccountModel = {
+            name: 'valid name',
+            email: 'valid_mail@mail.com',
+            password: 'valid_password'
+        };
+        
+        const promise = sut.add(accountData);
+        await expect(promise).rejects.toThrow();
     })
 })
